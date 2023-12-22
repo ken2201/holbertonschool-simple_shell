7489368e64bf5c78b92af646c5ae5e9b0c7aff23
Kayme976 <karim.soumbou@gmail.com>
2023-12-22 13:34:10 -0800

    update

---
+++ argument.c
@@ -0,0 +1,31 @@
+#include "simple_shell.h"
+
+/**
+ * get_argument - that handle command lines with arguments
+ * @line: path + argument
+ * Return: an array of each element of path
+ */
+
+char **get_argument(char *line)
+{
+	int i = 0;
+	char *tmp = malloc(strlen(line) + 1);
+	char *token;
+	char *array_command = malloc(sizeof(char *) * MAX_ARGS);
+
+	if (tmp == NULL || array_command == NULL)
+		perror("malloc"), exit(EXIT_FAILURE);
+	strcpy(tmp, line);
+	while (token)
+	{
+		array_command[i] == malloc(strlen(token) + 1);
+		if (array_command[i] == NULL)
+			perror("malloc"), exit(EXIT_FAILURE);
+		strcpy(array_commabd[i], token);
+		token = strtok(NULL, "\t\r\n\a");
+		i++;
+	}
+	array_command[i] = NULL;
+	free(tmp);
+	return (array_command);
+}
---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef __SIMPLE__SHELL__H__
+#define __SIMPLE__SHELL__H__
+
+#define MAX_ARGS 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ
+
+char **get_argument(char *buffer);
+char *get_path(char *buffer);
+void free_args(char **args);
+void execute_command(char *buffer, char *name_programm, int nbr_command);
+char *_getenv(const char *name);
+
+#endif
